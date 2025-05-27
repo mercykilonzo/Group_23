7eb77061b25e44ed2fe742c258de33f9eceb2451
Mercy Mwikali <mwikalimercykilonzo.com>
2025-05-27 13:41:39 +0300

    This folder contains the implementations of conveyor belt system in kotlin, javascript and python

---
+++ Question1/Conveyor_belt/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+
+    fun main() {
+        val conveyor = (conveyorBelt("scarf"))
+        println(conveyor)
+    }
+    fun conveyorBelt(item:String):String{
+        var dequeuedItem = ""
+        var conveyorList = mutableListOf<String>("bag","shoes")
+
+        conveyorList.add(item)
+        if (conveyorList.isNotEmpty()){
+            dequeuedItem = conveyorList.removeAt(0)
+        }
+        return dequeuedItem
+
+    }
